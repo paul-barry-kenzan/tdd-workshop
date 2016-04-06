e3920a3c35722da8158f023490265716f3f0da71
Paul Barry <pbarry@kenzan.com>
2016-04-06 00:31:31 -0400

    Spec & Code 1

--- src/modules/articles/services/articles-service.spec.js
+++ src/modules/articles/services/articles-service.spec.js
@@ -16,7 +16,11 @@ describe('Articles Service', function () {
     expect(ArticleService).to.exist();
   });
 
-  it('should expose a getArticles method');
+  it('should expose a getArticles method', function () {
+    expect(ArticleService.getArticles).to.not.undefined();
+    expect(angular.isFunction(ArticleService.getArticles)).to.be.true();
+  });
+
   it('should return a promise when calling getArticles');
   it('should $log a "Request Error! There was an error with the request." error if the server returns a 500 status');
 
