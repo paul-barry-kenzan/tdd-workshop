fe79495d275a39ce0426dd0ccdd4106553b609dc
Paul Barry <pbarry@kenzan.com>
2016-04-08 01:36:55 -0400

    Wiredeps for bower files

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,19 @@
 // Karma configuration
 // Generated on Fri Apr 08 2016 01:24:41 GMT-0400 (EDT)
+var wiredep = require('wiredep');
 
 module.exports = function(config) {
+
+  var bowerFiles = wiredep({devDependencies:true}).js;
+  var appFiles = [
+    './src/modules/**/*-module.js',
+    './src/modules/**/**/*.js'
+  ];
+
   config.set({
     basePath: './',
     frameworks: ['mocha'],
-    files: [
-      './src/modules/**/*-module.js',
-      './src/modules/**/**/*.js'
-    ],
+    files: [].concat(bowerFiles, appFiles),
     preprocessors: {},
     reporters: ['progress'],
     port: 9876,
